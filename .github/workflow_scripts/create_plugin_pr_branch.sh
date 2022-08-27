58a3b4a9f0fc8fe0c6b1b21e69f47e4070df4e73
Josh.5 <jsunnex@gmail.com>
2022-08-27 13:16:28 +1200

    Added some local debugging

--- .github/workflow_scripts/create_plugin_pr_branch.sh
+++ .github/workflow_scripts/create_plugin_pr_branch.sh
@@ -5,7 +5,7 @@
 # File Created: Friday, 26th August 2022 8:17:10 pm
 # Author: Josh.5 (jsunnex@gmail.com)
 # -----
-# Last Modified: Friday, 26th August 2022 8:59:59 pm
+# Last Modified: Saturday, 27th August 2022 1:27:34 pm
 # Modified By: Josh.5 (jsunnex@gmail.com)
 ###
 #
@@ -13,11 +13,10 @@
 #
 ###
 
-official_git_repo="git@github.com:Unmanic/unmanic-plugins.git"
-repo_root_path=$(readlink -e $(dirname "${BASH_SOURCE[0]}")/../../)
-
 plugin_id="${@}"
+repo_root_path=$(readlink -e $(dirname "${BASH_SOURCE[0]}")/../../)
 plugin_location="${repo_root_path}/build/plugin.${plugin_id}"
+mkdir -p "${repo_root_path}/build"
 
 echo ${plugin_location}
 ls -la ${plugin_location}
@@ -28,11 +27,23 @@ if [[ -z ${plugin_id} ]]; then
 fi
 
 
+# DEBUGGING - Clone plugin to temp directory
+if [[ ! -z ${script_debugging} ]]; then
+    git checkout master
+    if [[ ! -d ${plugin_location} ]]; then
+        echo -e "\n*** Cloning plugin git repo to '${tmp_dir}/${plugin_id}'"
+        git clone --depth=1 --branch master --single-branch "git@github.com:Unmanic/plugin.${plugin_id}" "${plugin_location}"
+        [[ $? -gt 0 ]] && echo "Failed to fetch the plugin git repository. Exit!" && exit 1;
+    fi
+fi
+
+
 ########################################################################
 ### CREATE PR BRANCH
 pushd "${repo_root_path}" &> /dev/null
-# Create PR branch 
-echo -e "\n*** Checkout PR branch for plugin"
+# Create clean PR branch 
+echo -e "\n*** Checkout clean PR branch for plugin"
+git branch -D "pr-${plugin_id}" 2> /dev/null
 git checkout -b "pr-${plugin_id}"
 popd &> /dev/null
 
@@ -71,6 +82,7 @@ rsync -avh --delete \
 # Read plugin version
 plugin_version=$(cat ${plugin_location}/info.json | jq -rc '.version')
 [[ ${plugin_version} == "null" ]] && echo "Failed to fetch the plugin's version from the info.json file. Exit!" && exit 1;
+# TODO: Remove "${plugin_location}/"
 popd &> /dev/null
 
 # TODO: Remove logging
@@ -83,7 +95,7 @@ pushd "${repo_root_path}" &> /dev/null
 echo -e "\n*** Commit changes in unmanic-plugins repository"
 commit_message="[${plugin_id}] v${plugin_version}"
 echo ${commit_message}
-git add .
+git add "source/${plugin_id}"
 git commit -m "${commit_message}"
 if [[ $? -gt 0 ]]; then
     echo
